3f9b57817e792942b399825406e633f72aab422e
GreatChenLab <90399926+GreatChenLab@users.noreply.github.com>
2026-06-11 08:35:55 +0800

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -9,7 +9,6 @@ import pickle
 import os
 
 def load_model(model_class, model_path, model_params, device):
-    """加载训练好的模型"""
     model = model_class(**model_params)
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
@@ -17,9 +16,6 @@ def load_model(model_class, model_path, model_params, device):
     return model
 
 def make_predictions(model, predict_loader, class_names, device, threshold=0.5):
-    """执行预测并返回概率和最终类别"""
-    print(f"预测DataLoader中包含 {len(predict_loader)} 个批次。")
-
     all_probs = []
     all_final_classes = []
 
@@ -27,7 +23,7 @@ def make_predictions(model, predict_loader, class_names, device, threshold=0.5):
         batch_count = 0
         for batch_data, fp in predict_loader:
             batch_count += 1
-            print(f"正在处理第 {batch_count} 个批次...") # 【调试输出4】
+            print(f"The {batch_count} batch is being processed...")
         
             batch_data, fp = batch_data.to(device), fp.to(device)
             out = model(batch_data, fp)
@@ -42,14 +38,12 @@ def make_predictions(model, predict_loader, class_names, device, threshold=0.5):
                 all_final_classes.append(','.join(pred_class_names))
 
     if not all_probs:
-        print("错误：预测循环未生成任何概率数据。")
+        print("Error: The prediction loop did not generate any probability data.")
     return all_probs, all_final_classes
 
 def evaluate_and_save_results(all_probs, all_final_classes, valid_smiles, 
                               class_names, output_path):
-    """保存结果到CSV"""
     all_probs = np.vstack(all_probs)
-    # -------------------- 保存最终结果 --------------------
     result_dict = {}
     for i, cls_name in enumerate(class_names):
         result_dict[f'{cls_name}_prob'] = all_probs[:, i]
@@ -58,5 +52,5 @@ def evaluate_and_save_results(all_probs, all_final_classes, valid_smiles,
 
     result_df = pd.DataFrame(result_dict)
     result_df.to_csv(output_path, index=False, encoding='utf-8-sig')
-    print(f"\n预测结果已保存至：{output_path}")
+    print(f"\nThe prediction results have been saved to：{output_path}")
     pass
